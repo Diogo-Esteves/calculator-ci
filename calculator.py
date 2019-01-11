7d76d267f4fafc801311de1b71ebadd94f1c9a4d
Diogo-Esteves <diogoesteves>
2019-01-11 15:42:29 -0200

    making the tests pass

--- calculator.py
+++ calculator.py
@@ -4,11 +4,14 @@
 Calculator containing olny the basics math operations.
 """
 
+
 def add(first_term, second_term):
     return first_term + second_term
 
+
 def subtract(first_term, second_term):
     return first_term - second_term
+
 
 def multiply(first_term, second_term):
     return first_term * second_term
--- test_calculato.py
+++ test_calculato.py
@@ -1,13 +1,14 @@
 
 
 """
-Unit tests for the calculator 
+Unit tests for the calculator
 """
 
 import calculator
 
 
 class TestCalculator:
+
 
     def test_addition(self):
         assert 6 == calculator.add(4, 2)
@@ -16,4 +17,4 @@ class TestCalculator:
         assert 0 == calculator.subtract(4, 4)
 
     def test_multiplication(self):
-        assert 9 == calculator.multiply(3,3)
+        assert 9 == calculator.multiply(3, 3)
