71dd0c1c713e375246044723547894eb2e5508c0
Diogo-Esteves <diogoesteves>
2019-01-11 15:14:04 -0200

    First commit, up the files

---
+++ calculator.py
@@ -0,0 +1,10 @@
+
+"""
+Calculator containing olny the basics math operations.
+"""
+
+def add(first_term, second_term):
+    return first_term + second_term
+
+def subtract(first_term, second_term):
+    return first_term - second_term
