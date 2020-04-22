aad73ddba626165ea913c191bf45575797895eef
Diogo Esteves dos Santos <diogoesteves_skt@hotmail.com>
2020-04-22 12:01:25 -0300

    making the test fail

---
+++ test_calculator.py
@@ -0,0 +1,19 @@
+
+
+"""
+Unit tests for the calculator
+"""
+
+import calculator
+
+
+class TestCalculator:
+
+    def test_addition(self):
+        assert 6 == calculator.add(4, 2)
+
+    def test_subtraction(self):
+        assert 0 == calculator.subtract(4, 4)
+
+    def test_multiplication(self):
+        assert 100 == calculator.multiply(10, 9)
